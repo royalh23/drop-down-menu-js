dafb74e579e7f91369b341219737565d57ff3baf
Royal <royalhuseyn902@gmail.com>
2023-12-31 11:24:03 +0400

    Write JS logic for revealing/closing menu upon clicks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.css';
+
+function displayMenu(menuBtnClass, menuClass, closeBtnClass) {
+  const menuBtn = document.querySelector(`.${menuBtnClass}`);
+  const menu = document.querySelector(`.${menuClass}`);
+  const closeBtn = document.querySelector(`.${closeBtnClass}`);
+
+  menuBtn.addEventListener('click', () => {
+    menu.style.visibility = 'visible';
+  });
+
+  closeBtn.addEventListener('click', () => {
+    menu.style.visibility = 'hidden';
+  });
+}
+
+displayMenu('menu-icon', 'menu', 'close-icon');
