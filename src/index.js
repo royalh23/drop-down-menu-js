df8f01f0c3462c359b6aed9874c20fc5aada3f21
Royal <royalhuseyn902@gmail.com>
2024-01-01 23:38:24 +0400

    Rename displayMenu to activateMenu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 
-function displayMenu(menuBtnClass, menuClass, closeBtnClass) {
+function activateMenu(menuBtnClass, menuClass, closeBtnClass) {
   const menuBtn = document.querySelector(`.${menuBtnClass}`);
   const menu = document.querySelector(`.${menuClass}`);
   const closeBtn = document.querySelector(`.${closeBtnClass}`);
@@ -10,4 +10,4 @@ function displayMenu(menuBtnClass, menuClass, closeBtnClass) {
   closeBtn.addEventListener('click', () => menu.classList.toggle('visible'));
 }
 
-displayMenu('menu-icon', 'menu', 'close-icon');
+activateMenu('menu-icon', 'menu', 'close-icon');
